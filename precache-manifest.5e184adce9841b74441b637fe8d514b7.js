1ed3487682f4ef0302b97e3702a4d2f27f7b7bab
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-05-25 22:34:57 +0530

    Updates

---
+++ precache-manifest.5e184adce9841b74441b637fe8d514b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd240c430170e1433c6755f630cfec83",
+    "url": "/BurgerBuilder-react/index.html"
+  },
+  {
+    "revision": "f9e347b4691c22d24d07",
+    "url": "/BurgerBuilder-react/static/css/main.1d77c2ce.chunk.css"
+  },
+  {
+    "revision": "c96c5ac96555c9c35979",
+    "url": "/BurgerBuilder-react/static/js/2.bb65fa8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerBuilder-react/static/js/2.bb65fa8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9e347b4691c22d24d07",
+    "url": "/BurgerBuilder-react/static/js/main.8e618471.chunk.js"
+  },
+  {
+    "revision": "1a579178d5d05bb94596",
+    "url": "/BurgerBuilder-react/static/js/runtime-main.5cc61886.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/BurgerBuilder-react/static/media/burger-logo.b8503d26.png"
+  }
+]);

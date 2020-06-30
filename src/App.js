348cf4cbcb2dc426aa597ed6cd3cef930f1ba8bb
Pulsara Sandeepa <pulsarasandeepa@gmail.com>
2020-07-01 01:19:36 +0530

    Created the checkot page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, {Component} from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuild from './containers/BurgerBuilder/BurgerBuild';
+import Checkout from "./containers/Checkout/Checkout";
 class App extends Component{
 render(){
         return (
             <Layout>
                <BurgerBuild/>
+               <Checkout/>
             </Layout>
         );
- 
+
     }
 }
 
